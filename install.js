4391a3d1e8b4d802cfab11072091f7e3df8808c3
SUP3RMASS1VE <Martincull@live.com>
2024-09-25 21:39:30 +0100

    Update install.js

--- install.js
+++ install.js
@@ -5,22 +5,11 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/runew0lf/RuinedFooocus.git app",
+          "git clone https://github.com/mcmonkeyprojects/SwarmUI app",
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
+    
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -28,60 +17,68 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "",
+          
         ]
       }
     },
     {
-      "method": "fs.link",
+      "method": "shell.run",
+      "params": {
+        "message": [
+          "git clone https://github.com/SUP3RMASS1VE/Models",
+        ],
+        "path": "app"
+      }
+    },
+    {
+      "method": "shell.run",
+      "params": {
+        "message": [
+          "git clone https://github.com/SUP3RMASS1VE/Output",
+        ],
+        "path": "app"
+      }
+    },
+    {
+      "method": "fs.share",
       "params": {
         "drive": {
-          "checkpoints": "app/models/checkpoints",
-          "clip": "app/models/clip",
+          "checkpoints": "app/Models/Stable-Diffusion/OfficialStableDiffusion",
+          "clip": "app/Models/clip",
           "clip_vision": "app/models/clip_vision",
-          "configs": "app/models/configs",
-          "controlnet": "app/models/controlnet",
-          "embeddings": "app/models/embeddings",
-          "loras": "app/models/loras",
-          "upscale_models": "app/models/upscale_models",
-          "vae": "app/models/vae",
-          "vae_approx": "app/models/VAE-approx",
-          "diffusers": "app/models/diffusers",
-          "unet": "app/models/unet",
-          "vae_approx": "app/models/vae_approx",
-          "hypernetworks": "app/models/hypernetworks",
-          "gligen": "app/models/gligen",
-          "style_models": "app/models/style_models",
-          "photomaker": "app/models/photomaker"
+          "configs": "app/Models/configs",
+          "controlnet": "app/Models/controlnet",
+          "embeddings": "app/Models/Embeddings",
+          "loras": "app/Models/Lora",
+          "upscale_models": "app/Models/upscale_models",
+          "vae": "app/Models/VAE",
+          "vae_approx": "app/Models/VAE-approx",
+          "diffusers": "app/Models/diffusers",
+          "unet": "app/Models/unet",
+          "vae_approx": "app/Models/vae_approx",
+          "hypernetworks": "app/Models/hypernetworks",
+          "gligen": "app/Models/gligen",
+          "style_models": "app/Models/style_models",
+          "photomaker": "app/Models/photomaker",
+          "outputs": "app/Output"
         },
         "peers": [
           "https://github.com/cocktailpeanutlabs/automatic1111.git",
           "https://github.com/cocktailpeanutlabs/fooocus.git",
           "https://github.com/cocktailpeanutlabs/comfyui.git",
           "https://github.com/pinokiofactory/stable-diffusion-webui-forge.git",
-          "https://github.com/SUP3RMASS1VE/RuinedFooocus.git"
+          "https://github.com/SUP3RMASS1VE/RuinedFooocus.git",
+          "https://github.com/SUP3RMASS1VE/SwarmUI.git",
         ]
       }
     },
     {
-      "method": "fs.link",
-      "params": {
-        "drive": {
-          "output": "app/output"
-        }
+      method: "fs.link",
+      params: {
+        venv: "app/env"
       }
     },
-    {
-      method: "shell.run",
-      params: {
-        message: "{{'launch.py'}}",
-        env: {
-          SD_WEBUI_RESTARTING: 1,
-        },
-        path: "app",
-        on: [{ "event": "/http:\/\/[0-9.:]+/", "kill": true }]
-      }
-    }
   ]
 }
+
 
