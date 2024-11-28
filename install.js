b9ef8b79730bf8e2f6d76380ca48213828f585e9
SUP3RMASS1VE <Martincull@live.com>
2024-11-28 20:08:10 +0000

    Update install.js

--- install.js
+++ install.js
@@ -28,8 +28,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio",
           "pip install -r requirements_versions.txt",
+          "pip install -r pip_modules.txt"
         ]
       }
     },
@@ -72,6 +72,12 @@ module.exports = {
           "https://github.com/cocktailpeanutlabs/comfyui.git",
           "https://github.com/pinokiofactory/stable-diffusion-webui-forge.git"
         ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
       }
     },
   ]
